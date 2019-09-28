90b684e7efbbb86a380e5976dcbbf3f4e4cdc36f
lane <wulanruo@yeah.net>
2019-09-28 10:57:25 +0800

    Update Mode.java

--- 算法-课堂题目/src/pers/lane/algorithm/work/Mode.java
+++ 算法-课堂题目/src/pers/lane/algorithm/work/Mode.java
@@ -1,29 +1,44 @@
 package pers.lane.algorithm.work;
-import pers.lane.sort.*;
-import java.util.ArrayList;
+import java.util.Arrays;
+
+import pers.lane.sort.QuickSort;
 
 //众数-分治法
+//不允许出现重复的众数
 public class Mode {
-	static int A[]=new int[100];
+
 public static void main(String[] args) {
-	QuickSort qc=new QuickSort();
-	qc.quicksort1(A,L,R);
+	int [] A= new int[20];
+	for(int i=0;i<20;i++)
+	{
+		A[i]=(int) (Math.random()*10+1);
+	}
+	QuickSort.quicksort1(A,0,A.length-1);
+	System.out.println("随机生成的数组如下：");
+	System.out.println(Arrays.toString(A));
+	System.out.println("众数为:"+A[func(A,0,A.length-1)[0]]);
+	System.out.println("众数的重数为: "+func(A,0,A.length-1)[1]);
+	
+//	int []B= {1, 1, 1, 1, 1, 1, 2, 3, 3, 4, 5, 6, 6, 6, 6, 8, 8, 10, 10, 10};
+//	System.out.println("众数为: "+func(B,0,B.length-1)[1]);
 }
 
 
 
-public static int func(int[] A,int L, int R)//取中间的数做基准
+public static int[] func(int[] A,int L, int R)//取中间的数做基准
 {
-	
+	//不用显式出口，会越分越小的
+	int [] B=new int[2];//辅助数组B，B[0]放pos，B[1]放max
+	int pos;
 	int mid=L+(R-L)/2;
-	int count=0;
-
+	int pos2=0;//子序列得到的pos结果
+	int count=0;//子序列得到的max结果
+	int max=0;//最大重数；
    int X=A[mid];
    int right=0;//记下第一个X
    int left = 0;//记下最后一个X
-   int i=0;//指针
-//扫描A,看有无与X相同的数，与X周围交换，但是这样不能保证X们还在中间，所以还是得排个序
-  for(;i<A.length;i++)
+   int i;
+  for(i=L;i<=R;i++)
   {
 	  if(A[i]==X)
 	  {
@@ -31,28 +46,37 @@ public static int func(int[] A,int L, int R)//取中间的数做基准
 		 break;
 	  }
   }
-  while(i<A.length)
+  for(;i<=R;i++)
   {
-	  i++;
 	  if(A[i]!=X)
 		  break;
   }
  right=i;//右半段的起始
- if((left-L+1)>(right-left-1))//如果左边的数，比中间数的个数要小就不用扫描左边了了
+ max=right-left-1;//中间数的重数
+ pos=mid;
+ if((left-L+1)>max)//如果左边的数，比中间数的个数要小就不用扫描左边了了
 		 {
-	          func(A,L,left);
+	          count=func(A,L,left)[1];
+	          pos2=func(A,L,left)[0];
+	          if(max<count)
+	          {
+	        	  pos=pos2;
+	        	  max=count;
+	          }
          }
- if((R-right+1)>(right-left-1))//如果右边、边的数，比中间数的个数要小就不用扫描右边了了
+ if((R-right+1)>max)//如果右边、边的数，比中间数的个数要小就不用扫描右边了了
  {
-	 func(A,right,R);
+	 count=func(A,right,R)[1];
+	 pos2=func(A,right,R)[0];
+	 if(max<count)
+	 {
+		 pos=pos2;
+		 max=count;
+	 }
  }
- if(((left-L+1)<(right-left-1))&&((R-right+1)<(right-left-1)))
- {
-	 
- }
- 
- 
- 
+         B[0]=pos;
+         B[1]=max;
+          return B;
 }
 
 
